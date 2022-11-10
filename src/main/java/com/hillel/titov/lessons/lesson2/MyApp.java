275a2cf489941a5b7d8d5a9c008fea8c76bc7193
Dmytro Titov <octopusless@gmail.com>
2022-11-10 20:08:37 +0200

    Add project structure

---
+++ src/main/java/com/hillel/titov/lessons/lesson2/MyApp.java
@@ -0,0 +1,11 @@
+package com.hillel.titov.lessons.lesson2;
+
+public class MyApp {
+    public static void main(String[] args) {
+        double latitude = 50.449551299999996;
+        double longitude = 30.525385799999995;
+
+        System.out.format("According to Google Maps, the Monument of Independence is located at %.15f, %.15f.", latitude, longitude);
+
+    }
+}
---
+++ src/main/java/com/hillel/titov/lessons/lesson3/Main.java
@@ -0,0 +1,7 @@
+package com.hillel.titov.lessons.lesson3;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
